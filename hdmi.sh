d6d682f668dc2691c92237c3f31fdf0b61b08dcb
Jakub Gladykowski <gladykov@gmail.com>
2024-05-17 16:17:23 -0300

    Update hdmi.sh

--- hdmi.sh
+++ hdmi.sh
@@ -5,11 +5,14 @@ export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 export XDG_RUNTIME_DIR="/run/user/1000"
 
+# Wait for Udev worker to finish processing all events
+sleep 5
+
 if pactl list | grep "available: yes" | grep hdmi
     then
-    echo "HDMI disconnected"
-    pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
-else
     echo "HDMI connected"
     pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo+input:analog-stereo
+else
+    echo "HDMI disconnected"
+    pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
 fi
